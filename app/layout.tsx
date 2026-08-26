d842aecc54ce28f3b766a388592569c8ad63001f
MelyC2 <melanny.castillo@ister.edu.ec>
2026-08-26 17:21:02 -0500

    fix: actualización layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
 import type { Metadata } from "next";
-
 import "./globals.css";
-
 import Navbar from "@/components/Navbar";
 
-
 export const metadata: Metadata = {
-  title: "DevPort - Portal de empleos tech",
-  description: "Encuentra proyectos y empleos tech",
+  title: "TripNova - Plataforma de viajes",
+  description: "Descubre destinos y planifica tus próximas aventuras.",
 };
 
 export default function RootLayout({
@@ -16,12 +13,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="es"
-    >
+    <html lang="es">
       <body className="bg-slate-950 text-white min-h-screen">
         <Navbar />
-        <main>{children} </main>
+        <main>{children}</main>
       </body>
     </html>
   );
